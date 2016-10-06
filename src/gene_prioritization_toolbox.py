ae7f1869f4ca82f618f9fcbf399241e73f62fb8e
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2016-10-06 16:19:59 -0500

    lint comments cleanup pearson complete

--- src/gene_prioritization_toolbox.py
+++ src/gene_prioritization_toolbox.py
@@ -11,10 +11,14 @@ import knpackage.toolbox as kn
 
 def perform_pearson_correlation(spreadsheet, drug_response):
     """ Find pearson correlation coefficient(PCC) for each gene expression (spreadsheet row)
-    with the drug response.
+            with the drug response.
+
     Args:
         spreadsheet: genes x samples gene expression data
         drug_response: one drug response for each sample
+
+    Returns:
+        pc_array: row-spreadsheet-features-ordered array of pearson correlation coefficients
     """
     pc_array = np.zeros(spreadsheet.shape[0])
     for row in range(0, spreadsheet.shape[0]):
@@ -23,12 +27,15 @@ def perform_pearson_correlation(spreadsheet, drug_response):
 
     return pc_array
 
+
 def run_gene_correlation(run_parameters):
-    ''' wrapper: call sequence to perform gene prioritization
+    ''' pearson cc:  call sequence to perform gene prioritization
+
     Args:
         run_parameters: dict object with keys:
                 run_parameters["spreadsheet_name_full_path"]
                 run_parameters["drug_response_full_path"]
+
     Returns:
         result_df:  result - dataframe of gene prioritization. Values are pearson
                     correlation coefficient values in descending order.
@@ -42,8 +49,9 @@ def run_gene_correlation(run_parameters):
 
     return
 
+
 def perform_gene_correlation(run_parameters):
-    """ read the files named in run_parameters and find the pearson correlation coefficient per gene
+    """ find the pearson correlation coefficient per gene
 
     Args:
         run_parameters: dict with keys:
@@ -59,8 +67,9 @@ def perform_gene_correlation(run_parameters):
                              columns=['PCC']).abs().sort_values("PCC", ascending=0)
     return result_df
 
+
 def run_net_correlation(run_parameters):
-    ''' wrapper: call sequence to perform gene prioritization
+    ''' pearson cc:  call sequence to perform gene prioritization with network smoothing
 
     Args:
         run_parameters: dict object with keys:
@@ -72,12 +81,6 @@ def run_net_correlation(run_parameters):
         result_df: result dataframe of gene prioritization. Values are pearson
         correlation coefficient values in descending order.
     '''
-    #spreadsheet_df, sample_smooth = get_smooth_spreadsheet_matrix(run_parameters)
-    #drug_response = kn.get_spreadsheet_df(run_parameters["drug_response_full_path"])
-    #pc_array = perform_pearson_correlation(sample_smooth, drug_response.values[0])
-    #result_df = pd.DataFrame(pc_array, index=spreadsheet_df.index.values,
-                             #columns=['PCC']).abs().sort_values("PCC", ascending=0)
-
     result_df = perform_net_correlation(run_parameters)
     target_file_base_name = "gene_drug_network_correlation"
     target_file_base_name = os.path.join(run_parameters["results_directory"], target_file_base_name)
@@ -87,8 +90,9 @@ def run_net_correlation(run_parameters):
 
     return
 
+
 def perform_net_correlation(run_parameters):
-    ''' wrapper: call sequence to perform gene prioritization
+    ''' perform gene prioritization with network smoothing and Pearson correlation
 
     Args:
         run_parameters: dict object with keys:
@@ -111,7 +115,8 @@ def perform_net_correlation(run_parameters):
 
 
 def run_bootstrap_correlation(run_parameters):
-    """ wrapper: call sequence to perform gene prioritization using bootstrap sampling
+    """ pearson cc: call sequence to perform gene prioritization using bootstrap sampling
+
     Args:
         run_parameters: dict object with keys:
             run_parameters["spreadsheet_name_full_path"]    (samples x genes spreadsheet)
@@ -130,8 +135,10 @@ def run_bootstrap_correlation(run_parameters):
 
     return
 
+
 def perform_bootstrap_correlation(run_parameters):
-    """ wrapper: call sequence to perform gene prioritization using bootstrap sampling
+    """ perform gene prioritization using bootstrap sampling with Pearson correlation
+
     Args:
         run_parameters: dict object with keys:
             run_parameters["spreadsheet_name_full_path"]    (samples x genes spreadsheet)
@@ -165,7 +172,8 @@ def perform_bootstrap_correlation(run_parameters):
 
 
 def run_bootstrap_net_correlation(run_parameters):
-    """ wrapper: call sequence to perform gene prioritization using bootstrap sampling and network smoothing
+    """ pearson cc: call sequence to perform gene prioritization using bootstrap sampling and network smoothing
+
     Args:
         run_parameters: dict object with keys:
             run_parameters["spreadsheet_name_full_path"]    (samples x genes spreadsheet)
@@ -176,25 +184,6 @@ def run_bootstrap_net_correlation(run_parameters):
         result_df: result dataframe of gene prioritization. Values are pearson
         correlation coefficient values in descending order.
     """
-    #spreadsheet_df, sample_smooth = get_smooth_spreadsheet_matrix(run_parameters)
-    #drug_response = kn.get_spreadsheet_df(run_parameters["drug_response_full_path"])
-
-    #borda_count = np.int_(np.zeros(sample_smooth.shape[0]))
-    #for bootstrap_number in range(0, int(run_parameters["number_of_bootstraps"])):
-        #sample_random, sample_permutation = kn.sample_a_matrix(
-            #sample_smooth, np.float64(run_parameters["rows_sampling_fraction"]),
-            #np.float64(run_parameters["cols_sampling_fraction"]))
-
-        #D = drug_response.values[0, None]
-        #D = D[0, sample_permutation]
-        #pc_array = perform_pearson_correlation(sample_random, D)
-        #pc_array_idx = np.argsort(pc_array)[::-1]
-        #borda_count = sum_vote_perm_to_borda_count(borda_count, pc_array_idx)
-
-    #borda_count = borda_count / max(borda_count)
-    #result_df = pd.DataFrame(borda_count, index=spreadsheet_df.index.values,
-                             #columns=['PCC']).abs().sort_values("PCC", ascending=0)
-
     result_df = perform_bootstrap_net_correlation(run_parameters)
     target_file_base_name = "gene_drug_network_bootstrap_correlation"
     target_file_base_name = os.path.join(run_parameters["results_directory"], target_file_base_name)
@@ -204,8 +193,9 @@ def run_bootstrap_net_correlation(run_parameters):
 
     return
 
+
 def perform_bootstrap_net_correlation(run_parameters):
-    """ wrapper: call sequence to perform gene prioritization using bootstrap sampling and network smoothing
+    """ perform gene prioritization using bootstrap sampling and network smoothing before Pearson correlation
     Args:
         run_parameters: dict object with keys:
             run_parameters["spreadsheet_name_full_path"]    (samples x genes spreadsheet)
@@ -214,7 +204,7 @@ def perform_bootstrap_net_correlation(run_parameters):
 
     Returns:
         result_df: result dataframe of gene prioritization. Values are pearson
-        correlation coefficient values in descending order.
+                    correlation coefficient values in descending order.
     """
     spreadsheet_df, sample_smooth = get_smooth_spreadsheet_matrix(run_parameters)
     drug_response = kn.get_spreadsheet_df(run_parameters["drug_response_full_path"])
@@ -237,13 +227,14 @@ def perform_bootstrap_net_correlation(run_parameters):
 
     return result_df
 
+
 def sum_vote_perm_to_borda_count(borda_count, vote_rank, vote_perm=None):
     """ incrementally update count by borda weighted vote in a subsample of the full borda size
 
     Args:
         vote_rank: (np.int_(vote_rank)) python rank array same size as borda_count (0 == first, 1 == second,...)
         borda_count: (np.int_(borda_count) ) the current running total of Borda weighted votes
-        vote_perm: the sample permutation of the vote ranking
+        vote_perm: (None) or: the sample permutation of the vote ranking
 
     Returns:
         borda_count: input borda_count with borda weighted vote rankings added
@@ -256,17 +247,19 @@ def sum_vote_perm_to_borda_count(borda_count, vote_rank, vote_perm=None):
 
     return borda_count
 
+
 def get_smooth_spreadsheet_matrix(run_parameters):
-    """ common task collection
+    """ common task collection for reading files and performaing random walk with restart
+
     Args:
         run_parameters: dict with keys:
                         spreadsheet_name_full_path
                         gg_network_name_full_path
     Returns:
-        sample_smooth:
+        spreadsheet_df: the spreadsheet dataframe
+        sample_smooth: the spreadsheet matrix smoothed with the network
     """
     spreadsheet_df = kn.get_spreadsheet_df(run_parameters["spreadsheet_name_full_path"])
-    #drug_response = kn.get_spreadsheet_df(run_parameters["drug_response_full_path"])
     network_df = kn.get_network_df(run_parameters['gg_network_name_full_path'])
     node_1_names, node_2_names = kn.extract_network_node_names(network_df)
     unique_gene_names = kn.find_unique_node_names(node_1_names, node_2_names)
@@ -286,23 +279,3 @@ def get_smooth_spreadsheet_matrix(run_parameters):
     sample_smooth, iterations = kn.smooth_matrix_with_rwr(spreadsheet_mat, network_mat, run_parameters)
 
     return spreadsheet_df, sample_smooth
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
