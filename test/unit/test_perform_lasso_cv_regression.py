4092310034f6729224be3bc584c53005b4df7e64
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2016-10-11 14:18:17 -0500

    variables renamed for clairity

--- test/unit/test_perform_lasso_cv_regression.py
+++ test/unit/test_perform_lasso_cv_regression.py
@@ -9,11 +9,11 @@ import gene_prioritization_toolbox as gptbx
 class TestPerform_lasso_cv_regression(TestCase):
 
     def test_perform_lasso_cv_regression(self):
-        spreadsheet = np.array([[0.8, 1.0, 0.4], [-0.3, 0.6, 0.1], [0.4, -0.1, 0.1], [0.4, -0.1, 0.1]])
-        drug_response = np.array([[0.5, 0.7, 0.1]])
+        samp_feat_matrix = np.array([[0.8, 1.0, 0.4], [-0.3, 0.6, 0.1], [0.4, -0.1, 0.1], [0.4, -0.1, 0.1]])
+        response_array = np.array([[0.5, 0.7, 0.1]])
         expected_lasso = np.array([0.9990000000000001, 0.0, -0.0, -0.0])
         expected_predictor = np.array([0.49993333, 0.69973333, 0.10033333])
-        lasso_array, lasso_predictor = gptbx.perform_lasso_cv_regression(spreadsheet, drug_response)
+        lasso_array, lasso_predictor = gptbx.perform_lasso_cv_regression(samp_feat_matrix, response_array)
 
         for n in range(0, expected_lasso.size):
             self.assertAlmostEqual(expected_lasso[n], lasso_array[n], msg='lasso coefficent unexpected')
