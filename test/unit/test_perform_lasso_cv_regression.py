e4fe3382bb2e668ff6a896dd112c1deabfe5c97a
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2016-10-12 17:07:52 -0500

    changed test data set

--- test/unit/test_perform_lasso_cv_regression.py
+++ test/unit/test_perform_lasso_cv_regression.py
@@ -13,13 +13,10 @@ class TestPerform_lasso_cv_regression(TestCase):
         response_array = np.array([[0.5, 0.7, 0.1]])
         expected_lasso = np.array([0.9990000000000001, 0.0, -0.0, -0.0])
         expected_predictor = np.array([0.49993333, 0.69973333, 0.10033333])
-        lasso_array, lasso_predictor = gptbx.perform_lasso_cv_regression(samp_feat_matrix, response_array)
+        lasso_array = gptbx.perform_lasso_cv_regression(samp_feat_matrix, response_array)
 
         for n in range(0, expected_lasso.size):
             self.assertAlmostEqual(expected_lasso[n], lasso_array[n], msg='lasso coefficent unexpected')
-
-        for n in range(0, expected_predictor.size):
-            self.assertAlmostEqual(expected_predictor[n], lasso_predictor[n], msg='lasso predictor unexpected')
 
 if __name__ == '__main__':
     unittest.main()
