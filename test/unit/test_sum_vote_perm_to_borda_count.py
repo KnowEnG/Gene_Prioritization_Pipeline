106092d57c5b9340eb8422e7385ea3e4d9f099b8
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2016-10-13 13:32:59 -0500

    updated unit test for sum_votes_to_borda_count

--- test/unit/test_sum_vote_perm_to_borda_count.py
+++ test/unit/test_sum_vote_perm_to_borda_count.py
@@ -9,34 +9,13 @@ import gene_prioritization_toolbox as gptbx
 class TestSum_vote_perm_to_borda_count(TestCase):
 
     def test_sum_vote_perm_to_borda_count(self):
-        borda_count = np.array([0,0,0,0,0])
-        vote_rank = np.array([0,3,1,2])
-        vote_perm = np.array([2,1,4,3])
-        borda_expected = np.array([0,1,4,2,3])
-        borda_returned = gptbx.sum_vote_perm_to_borda_count(borda_count, vote_rank, vote_perm)
-        for n in range(0, borda_expected.size):
-            self.assertEqual(borda_expected[n], borda_returned[n], msg='borda count unexpected')
 
-        vote_rank = np.array([1,0,2,3])
-        vote_perm = np.array([1,2,3,0])
-        borda_expected = np.array([1,4,8,4,3])
-        borda_returned = gptbx.sum_vote_perm_to_borda_count(borda_count, vote_rank, vote_perm)
-        for n in range(0, borda_expected.size):
-            self.assertEqual(borda_expected[n], borda_returned[n], msg='borda count unexpected')
-
-        vote_rank = np.array([2,0,1,3])
-        vote_perm = np.array([0,3,1,2])
-        borda_expected = np.array([3,7,9,8,3])
-        borda_returned = gptbx.sum_vote_perm_to_borda_count(borda_count, vote_rank, vote_perm)
-        for n in range(0, borda_expected.size):
-            self.assertEqual(borda_expected[n], borda_returned[n], msg='borda count unexpected')
-
-        borda_count = np.array([0,0,0,0])
-        vote_rank = np.array([0,3,1,2])
-        borda_expected = np.array([4,1,3,2])
-        borda_returned = gptbx.sum_vote_perm_to_borda_count(borda_count, vote_rank)
-        for n in range(0, borda_expected.size):
-            self.assertEqual(borda_expected[n], borda_returned[n], msg='borda count unexpected')
+        pc_arr = np.array([0.162876, 0.800422, 0.080408, 0.842713, 0.750278, 0.969880])
+        borda_count = np.zeros(pc_arr.size)
+        expected_result = np.array([2, 4, 1, 5, 3, 6])
+        borda_returned = gptbx.sum_vote_perm_to_borda_count(borda_count, pc_arr)
+        for n in range(0, expected_result.size):
+            self.assertEqual(expected_result[n], borda_returned[n], msg='borda count unexpected')
 
 if __name__ == '__main__':
     unittest.main()
